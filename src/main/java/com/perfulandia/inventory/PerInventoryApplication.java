a987acd04e2e9bff091486a8f60be62648d4d8f3
SepUC <mati.sepulvedam@duocuc.cl>
2025-05-13 12:33:08 -0400

    Inicio Proyecto

---
+++ src/main/java/com/perfulandia/inventory/PerInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfulandia/inventory/PerInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
