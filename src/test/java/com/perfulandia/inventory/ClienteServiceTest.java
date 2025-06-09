7edb456949ba4a63573c3898710cb4ff6e6b9aac
MrJeff22 <jefersoncarbonelldiaz@gmail.com>
2025-06-09 13:25:12 -0400

    Datafaker / Seguir instrucciones en REDAME

---
+++ src/test/java/com/perfulandia/inventory/ClienteServiceTest.java
@@ -0,0 +1,21 @@
+package com.perfulandia.inventory;
+
+import com.perfulandia.inventory.repository.ClienteRepository;
+import com.perfulandia.inventory.service.ClienteService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+public class ClienteServiceTest {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @MockBean
+    private ClienteRepository clienteRepository;
+
+    @Test
+    public void testfindClienteAll() {}
+}
